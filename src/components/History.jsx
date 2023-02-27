7e124c54e84bcadd2f698a0d224cbaa77219abe2
DeliaBarron <deliabarron.db@gmail.com>
2023-02-27 13:25:54 -0600

    Extra2: 'Toogle Moves Order' btn done! At History component

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react"
 
 export const History = ({currentMove, jumpToMove, history}) => {
- const [historyOrder, setHistoryOrder]=useState(true)//asc
- const [listOrder, setListOrder]=useState([])
+ const [order, setOrder]=useState(true)//asc
+ const [list, setList]=useState()
+let listArr
 
-// useEffect(()=>{
+useEffect(()=>{
+  renderList(history)
+  if(order){
+   setList(listArr)
+  // console.log(listArr)
+  } else if(order===false){
+    let sortedList=listArr.slice()
+    sortedList.reverse()
+    setList(sortedList)
+    console.log('sortedList key: ',sortedList[0].key)
+  }
+  console.log('list at uE call: ',list)
+  console.log('order at uE call:',order)
+},[order, history, currentMove])
 
-// },[historyOrder])
-
+  /*
   const moves=history.map((squares, moveIndex) => {
     let description
     console.log('history MAP ele/squares',squares, 'moveIndex: '+moveIndex)
@@ -26,29 +39,37 @@ export const History = ({currentMove, jumpToMove, history}) => {
       </li>
     )
 })
-
+*/
+function renderList(history){
+   listArr=history.map((squares, moveIndex)=>{
+    let description
+    console.log('history MAP ele/squares',squares, 'moveIndex: '+moveIndex)
+    if(moveIndex > 0){
+      description='Go to move: '+moveIndex
+    } else if(moveIndex===0){
+      description='Go to start game'
+    }
+    return (
+      <li key={moveIndex}>{
+        currentMove===moveIndex?
+          <button className="actual-move" onClick={() => jumpToMove(moveIndex)}>{'You are at move: '+ currentMove}</button>
+          :
+         <button className="jump-btn" onClick={() => jumpToMove(moveIndex)}>{description}</button>
+      }
+      </li>
+    )
+  })
+}
 
     function handleOrderToggle(){
-        let sortedMoves=moves
-        sortedMoves.reverse()
-        //LISTORDER ARRA
-        setListOrder(sortedMoves)
-        //SET TO ASC OR DESC
-        setHistoryOrder(!historyOrder)
-     }
+        setOrder(!order)
+    }
 
     
   return (
     <>
-    <button onClick={()=>{handleOrderToggle()}}>toggleOrder</button>
-    {/*
-     {
-        historyOrder ?
-        <ol> {moves}</ol>:
-        <ol>{listOrder}</ol>
-        } */}
-
-     <ol> {moves}</ol>
+    <button onClick={()=>{handleOrderToggle()}}>Toggle Moves Order</button>
+      <ol>{list}</ol> 
     </>
   )
 }
